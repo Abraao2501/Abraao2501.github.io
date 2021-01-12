90b19f837ad606502482efa908ad4461583d8e2b
Abraão Oliveira <abraaooliveira.eeq@gmail.com>
2021-01-12 10:52:18 -0300

    Picture carousel
    Creating the men's shoes page. A carousel of images of the shoes has been added to the pages. The page is not yet responsive and does not perform any functions through JS.

---
+++ scripts/mal-ids.js
@@ -0,0 +1,13 @@
+const imgs = [
+  (document.getElementById("1"),
+  document.getElementById("2"),
+  document.getElementById("3"),
+  document.getElementById("4"),
+  document.getElementById("5"),
+  document.getElementById("6"),
+  document.getElementById("7"),
+  document.getElementById("8"),
+  document.getElementById("9"))
+];
+
+
