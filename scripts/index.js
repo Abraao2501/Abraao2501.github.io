896c1e1c0760c5c0baa654171c08f5cb88648ae9
Abraão Oliveira <abraaooliveira.eeq@gmail.com>
2021-01-20 15:04:35 -0300

    Creating a data base of images
    pt.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
 const menuToggle = document.querySelector(".menu-toggle");
 const menuSection = document.querySelector(".menu-section");
+const file = nike
+
 
 //Using the "toggle" method to add / remove the "on" from the class list
 menuToggle.addEventListener("click", () => {
@@ -10,3 +12,6 @@ const comprar = document.querySelector('button')
 comprar.addEventListener('click' , () => {
   window.location.assign("./pages/info.html")
 })
+
+var image = document.querySelector('#shoe')
+image.src = '/imgs/arrow blue.png'
